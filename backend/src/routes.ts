65a698a48956eb33ade280abbe27084c8ed2f27d
zumzum00 <eliasdani96@gmail.com>
2022-05-11 09:07:16 +0200

    Correct the autentication to the user routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,10 +9,10 @@ export function getRouter(app:IRouter) {
   app.delete("/user/:id",[authJwt.verifyToken,authJwt.isAdmin], deleteUser);
   app.get("/users",[authJwt.verifyToken,authJwt.isAdmin], renderUsers);
 
-  app.get("/packages/:id",[authJwt.verifyToken,authJwt.isUser,authJwt.isAdmin], renderPackeges);
-  app.post("/newPackages",[authJwt.verifyToken,authJwt.isUser,authJwt.isAdmin], addPackage);
-  app.get("/package/:id",[authJwt.verifyToken,authJwt.isUser,authJwt.isAdmin], stornoParckage);
-  app.get("/createLabel/:id",[authJwt.verifyToken,authJwt.isUser,authJwt.isAdmin], generateLabel)
+  app.get("/packages/:id",[authJwt.verifyToken,authJwt.isUser], renderPackeges);
+  app.post("/newPackages",[authJwt.verifyToken,authJwt.isUser], addPackage);
+  app.get("/package/:id",[authJwt.verifyToken,authJwt.isUser], stornoParckage);
+  app.get("/createLabel/:id",[authJwt.verifyToken,authJwt.isUser], generateLabel)
 
   app.post("/auth/signup",signup)
   app.post("/auth/signin",signin);
